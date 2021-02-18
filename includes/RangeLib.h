e1df38686fd90698e019e514d14f946fb62172c2
droemer7 <droemer7@gmail.com>
2021-02-17 19:43:01 -0800

    Revert include

--- includes/RangeLib.h
+++ includes/RangeLib.h
@@ -28,9 +28,9 @@ Useful Links: https://github.com/MRPT/mrpt/blob/4137046479222f3a71b5c00aee1d5fa8
 #ifndef RANGE_LIB_H
 #define RANGE_LIB_H
 
-#include "../vendor/lodepng/lodepng.h"
-#include "../vendor/distance_transform.h"
-#include "RangeUtils.h"
+#include "vendor/lodepng/lodepng.h"
+#include "vendor/distance_transform.h"
+#include "includes/RangeUtils.h"
 
 #include <stdio.h>      /* printf */
 #include <cstdlib>
