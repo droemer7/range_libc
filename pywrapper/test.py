738c6bb0cbace645230902d2dda445037d676873
droemer7 <droemer7@gmail.com>
2021-02-19 21:27:53 -0800

    Update test.py to evaluate bugs

--- pywrapper/test.py
+++ pywrapper/test.py
@@ -5,12 +5,12 @@ import itertools, time
 ####################################################################################################
 #
 #                                              WARNING
-#  
+#
 #
 #                    This file uses range_libc in it's native coordinate space.
 #                      Use this method at your own peril since the coordinate
 #                      conversions are nontrivial from ROS's coordinate space.
-#                       Ignore this warning if you intend to use range_libc's 
+#                       Ignore this warning if you intend to use range_libc's
 #                                   left handed coordinate space
 #
 #
@@ -26,11 +26,11 @@ import itertools, time
 # print(range_libc.LRU_CACHE_SIZE)
 
 # testMap = range_libc.PyOMap("../maps/basement_hallways_5cm.png".encode('utf8'),1)
-testMap = range_libc.PyOMap("../maps/synthetic.map.png".encode('utf8'),1)
+testMap = range_libc.PyOMap("../maps/test.png".encode('utf8'),1)
 # testMap = range_libc.PyOMap("/home/racecar/racecar-ws/src/TA_examples/lab5/maps/basement.png".encode('utf8'),1)
 
 if testMap.error():
-	exit()
+  exit()
 # testMap.save("./loaded_map.png".encode('utf-8'))
 
 print("Initializing: bl")
@@ -46,78 +46,82 @@ print("")
 
 # For testing / debugging
 def fixed_scan(num_ranges, print_sample=True):
-	ranges_np = np.zeros(num_ranges, dtype=np.float32)		
-	queries = np.zeros((num_ranges, 3), dtype=np.float32)
-	queries[:,0] = testMap.width() / 2.0
-	queries[:,1] = testMap.height() / 2.0
-	queries[:,2] = np.linspace(0, 2.0 * np.pi, num_ranges)
+  ranges_np = np.zeros(num_ranges, dtype=np.float32)
+  queries = np.zeros((num_ranges, 3), dtype=np.float32)
+  queries[:,0] = testMap.width() / 2.0
+  queries[:,1] = testMap.height() / 2.0
+  queries[:,2] = np.linspace(0, 2.0 * np.pi, num_ranges)
+  queries_deg = np.copy(queries)
+  queries_deg[:,2] *= 180.0 / np.pi
+  print("Points (x, y, th): \n", queries_deg)
+  print()
 
-	if print_sample:
-		sample = np.random.random(5) * num_ranges
+  if print_sample:
+    sample = np.arange(0, num_ranges)
 
-	def scan(obj, name):
-		print("Running " + name + ":")
-		print("--------------------")
-		obj.calc_range_many(queries, ranges_np)	
-		ranges_slow = np.array([obj.calc_range(*q) for q in queries],
-		                       dtype=np.float32)
+  def scan(obj, name):
+    print("Running " + name + ":")
+    print("--------------------")
+    obj.calc_range_many(queries, ranges_np)
+    ranges_slow = np.array([obj.calc_range(*q) for q in queries],
+                           dtype=np.float32)
 
-		if print_sample:
-			print("Numpy sample:", [ranges_np[int(s)] for s in sample])
-			print("Slow sample: ", [ranges_slow[int(s)] for s in sample])
-		print("Diff:", np.linalg.norm(ranges_np - ranges_slow))
-		print("")
+    if print_sample:
+      print("Numpy sample:", [ranges_np[int(s)] for s in sample])
+      print("Slow sample: ", [ranges_slow[int(s)] for s in sample])
+    print("Diff:", np.linalg.norm(ranges_np - ranges_slow))
+    print("")
 
-		if range_libc.MAKE_TRACE_MAP and (name == "bl" or name == "rm"):
-			obj.saveTrace(str("./" + name + "_trace.png").encode('utf8'))
-	
-	scan(bl, "bl")
-	scan(rm, "rm")
-	scan(cddt, "cddt")
-	scan(glt, "glt")
+    if range_libc.MAKE_TRACE_MAP and (name == "bl" or name == "rm"):
+      obj.saveTrace(str("./" + name + "_trace.png").encode('utf8'))
+
+  scan(bl, "bl")
+  scan(rm, "rm")
+  scan(cddt, "cddt")
+  scan(glt, "glt")
 
 # For validation
 def random_scan(num_ranges):
-	for x in range(10):
-		ranges_np = np.zeros(num_ranges, dtype=np.float32)
-		queries = np.random.random((num_ranges, 3)).astype(np.float32)
-		queries[:,0] *= (testMap.width() - 2.0)
-		queries[:,1] *= (testMap.height() - 2.0)
-		queries[:,0] += 1.0
-		queries[:,1] += 1.0
-		queries[:,2] *= 2.0 * np.pi
+  for x in range(10):
+    ranges_np = np.zeros(num_ranges, dtype=np.float32)
+    queries = np.random.random((num_ranges, 3)).astype(np.float32)
+    queries[:,0] *= (testMap.width() - 2.0)
+    queries[:,1] *= (testMap.height() - 2.0)
+    queries[:,0] += 1.0
+    queries[:,1] += 1.0
+    queries[:,2] *= 2.0 * np.pi
 
-		def scan(obj, name):
-			print("Running " + name + ":")
-			print("--------------------")
-			
-			start = time.process_time()
-			obj.calc_range_many(queries, ranges_np)
-			end = time.process_time()
-			dur_np = end - start
-			print("Numpy: Computed", num_ranges, "ranges in", dur_np, "sec")
+    def scan(obj, name):
+      print("Running " + name + ":")
+      print("--------------------")
 
-			start = time.process_time()
-			ranges_slow = np.array([obj.calc_range(*q) for q in queries],
-		                       	   dtype=np.float32)
-			end = time.process_time()
-			dur_slow = end - start
-			print("Slow: Computed", num_ranges, "ranges in", dur_slow, "sec")
-			print("Numpy speedup:", dur_slow / dur_np)
+      start = time.process_time()
+      obj.calc_range_many(queries, ranges_np)
+      end = time.process_time()
+      dur_np = end - start
+      print("Numpy: Computed", num_ranges, "ranges in", dur_np, "sec")
 
-			diff = np.linalg.norm(ranges_np - ranges_slow)
-			print("Diff: {0}".format(diff))
-			if diff > 0.001:
-				print("Warning: Numpy result different from slow result,",
-				      "investigation possibly required")
-			else:
-			    print("Test passed")
-			print("")
+      start = time.process_time()
+      ranges_slow = np.array([obj.calc_range(*q) for q in queries],
+                                dtype=np.float32)
+      end = time.process_time()
+      dur_slow = end - start
+      print("Slow: Computed", num_ranges, "ranges in", dur_slow, "sec")
+      print("Numpy speedup:", dur_slow / dur_np)
 
-		scan(bl, "bl")
-		scan(rm, "rm")
-		scan(cddt, "cddt")
-		scan(glt, "glt")
+      diff = np.linalg.norm(ranges_np - ranges_slow)
+      print("Diff: {0}".format(diff))
+      if diff > 0.001:
+        print("Warning: Numpy result different from slow result,",
+              "investigation possibly required")
+      else:
+          print("Test passed")
+      print("")
 
-fixed_scan(num_ranges=100)
-random_scan(num_ranges=100000)
+    scan(bl, "bl")
+    scan(rm, "rm")
+    scan(cddt, "cddt")
+    scan(glt, "glt")
+
+fixed_scan(num_ranges=9)
+# random_scan(num_ranges=100000)
